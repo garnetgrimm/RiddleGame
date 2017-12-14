977637dfa9d1d9273bfc7ef50b9f5ea2053b3d25
TheOminousMango <fijichillz@gmail.com>
2017-12-13 20:01:18 -0500

    puzzle 2

--- app.py
+++ app.py
@@ -18,11 +18,19 @@ log.setLevel(logging.ERROR)
 app = Flask(__name__, static_url_path = "", static_folder = "static")
 
 @app.route('/')
-def hello_world():
-	return render_template("pboat827.html")
+def home():
+	return render_template("index.html")
 
-@app.route('/trySolutions', methods = ['POST'])
-def trySolutions():
+@app.route('/pboat287')
+def pboat():
+	return render_template("pboat287.html")
+
+@app.route('/ecar563')
+def ecar():
+	return render_template("ecar563.html")
+
+@app.route('/tryPBOAT', methods = ['POST'])
+def tryPboat():
 
 	s1 = request.form.get('s1')
 	s2 = request.form.get('s2')
@@ -35,6 +43,23 @@ def trySolutions():
 		r = True
 	else:
 		r = False
+	
+	response = jsonify(r)
+	response.headers.add('Access-Control-Allow-Origin', '*')	
+	return response
+	
+@app.route('/tryECAR', methods = ['POST'])
+def tryEcar():
+
+	s1 = request.form.get('s1')
+	
+	r = False
+	
+	if(s1 == "c"):
+		r = True
+	if(s1 == "C"):
+		r = True
+	
 	
 	response = jsonify(r)
 	response.headers.add('Access-Control-Allow-Origin', '*')	
--- autorun.bat
+++ autorun.bat
@@ -1,5 +1,5 @@
 set DATABASE_URL="postgresql://localhost/wordcount_dev"
-set FLASK_APP=serverside.py
+set FLASK_APP=app.py
 set FLASK_DEBUG=1
 flask run
 pause
